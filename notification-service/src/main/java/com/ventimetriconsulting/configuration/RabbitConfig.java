fe4548bc3993b3225910e64f08decc24965485c2
amatiangelo20m2 <amati.angelo@20m2official.it>
2024-05-30 00:56:07 +0200

    fix retry rabbit

--- notification-service/src/main/java/com/ventimetriconsulting/configuration/RabbitConfig.java
+++ notification-service/src/main/java/com/ventimetriconsulting/configuration/RabbitConfig.java
@@ -4,14 +4,15 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.retry.RetryPolicy;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class RabbitConfig {
@@ -32,10 +33,16 @@ public class RabbitConfig {
     }
 
     @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
     public SimpleRabbitListenerContainerFactory myFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setRetryTemplate(retryTemplate());
+        factory.setMessageConverter(producerJackson2MessageConverter());
         return factory;
     }
 
@@ -43,7 +50,7 @@ public class RabbitConfig {
     public RetryTemplate retryTemplate() {
         RetryTemplate retryTemplate = new RetryTemplate();
         FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
-        backOffPolicy.setBackOffPeriod(3000);
+        backOffPolicy.setBackOffPeriod(5000);
         retryTemplate.setBackOffPolicy(backOffPolicy);
         retryTemplate.setRetryPolicy(retryPolicy());
         return retryTemplate;
@@ -51,6 +58,6 @@ public class RabbitConfig {
 
     @Bean
     public RetryPolicy retryPolicy() {
-        return new SimpleRetryPolicy(3);
+        return new SimpleRetryPolicy(5);
     }
 }
